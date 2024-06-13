03808eb8cb6a40dcf4198f70ef429e6eb47c88c4
Kroxeal <zhenya.zaychenko.2017@mail.ru>
2024-06-14 02:18:35 +0300

    added class

--- dublicate_images/dublicate_imagess.py
+++ dublicate_images/dublicate_imagess.py
@@ -3,65 +3,65 @@ from PIL import Image
 import imagehash
 
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        if filename.lower().endswith((".jpeg", ".jpg")):
-            img_path = os.path.join(folder, filename)
-            try:
-                img = Image.open(img_path)
-                images.append((img_path, img))
-            except IOError:
-                print(f"Error loading image {img_path}")
-    return images
+class ImageDuplicateFinder:
+    def __init__(self):
+        self.images = []
+        self.image_hashes = {}
+        self.duplicates = {}
+
+    def load_images_from_folder(self, folder: str) -> None:
+        """
+        Loading images from a specified folder
+        Adding path and image_name into list images
+        """
+        for filename in os.listdir(folder):
+            if filename.lower().endswith((".jpeg", ".jpg")):
+                img_path = os.path.join(folder, filename)
+                try:
+                    img = Image.open(img_path)
+                    self.images.append((img_path, img))
+                except IOError:
+                    print(f"Error loading image {img_path}")
+
+    def calculate_image_hashes(self):
+        """Calculating perceptual hashes for loaded images"""
+        for img_path, img in self.images:
+            hash_val = imagehash.phash(img)
+            self.image_hashes[img_path] = hash_val
+
+    def find_duplicates(self):
+        """Finding duplicate images based on hashes"""
+        for img_path, hash_val in self.image_hashes.items():
+            if hash_val in self.duplicates:
+                self.duplicates[hash_val].append(img_path)
+            else:
+                self.duplicates[hash_val] = [img_path]
+
+        self.duplicates = {hash_val: img_paths for hash_val, img_paths in self.duplicates.items() if len(img_paths) > 1}
+
+    def print_duplicates(self):
+        """Printing the found duplicate images"""
+        for hash_val, img_paths in self.duplicates.items():
+            print(f"Duplicate hash: {hash_val}")
+            for img_path in img_paths:
+                print(f"\t{img_path}")
+
+    def process_folders(self, folder1:str, folder2:str =None):
+        """Process images from one or two folders"""
+        self.load_images_from_folder(folder1)
+        self.calculate_image_hashes()
+
+        if folder2:
+            self.load_images_from_folder(folder2)
+            self.calculate_image_hashes()
+
+        self.find_duplicates()
+        self.print_duplicates()
 
 
-def calculate_image_hashes(images):
-    image_hashes = []
-    for img_path, img in images:
-        hash_val = imagehash.phash(img)
-        image_hashes.append((img_path, hash_val))
-    return image_hashes
+if __name__ == "__main__":
+    folder1 = os.path.join("5 Flower Types Classification Dataset", "Lilly")
+    folder2 = os.path.join("5 Flower Types Classification Dataset", "Lotus")
 
-
-def find_duplicates(image_hashes):
-    duplicates = {}
-    hash_dict = {}
-
-    for img_path, hash_val in image_hashes:
-        if hash_val in hash_dict:
-            if hash_val not in duplicates:
-                duplicates[hash_val] = [hash_dict[hash_val]]
-            duplicates[hash_val].append(img_path)
-        else:
-            hash_dict[hash_val] = img_path
-
-    return duplicates
-
-
-def print_duplicates(duplicates):
-    for hash_val, img_paths in duplicates.items():
-        print(f"Duplicate hash: {hash_val}")
-        for img_path in img_paths:
-            print(f"\t{img_path}")
-
-
-def main(folder1, folder2=None):
-    images1 = load_images_from_folder(folder1)
-    image_hashes1 = calculate_image_hashes(images1)
-
-    if folder2:
-        images2 = load_images_from_folder(folder2)
-        image_hashes2 = calculate_image_hashes(images2)
-        image_hashes = image_hashes1 + image_hashes2
-    else:
-        image_hashes = image_hashes1
-
-    duplicates = find_duplicates(image_hashes)
-    print_duplicates(duplicates)
-
-
-folder1 = r"D:\downloads_chrome\5 Flower Types Classification Dataset\Lilly"
-folder2 = r"D:\downloads_chrome\5 Flower Types Classification Dataset\Rose"
-
-main(folder1, folder2)
+    finder = ImageDuplicateFinder()
+    finder.process_folders(folder1, folder2)
